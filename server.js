1e04f9d31da4d4d3a97536f70cd447d7f855a0f4
jeanyjsu <jeanyjsu@hotmail.com>
2024-11-30 20:19:53 -0800

    second commit

--- server.js
+++ server.js
@@ -12,6 +12,10 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+var whitelist = ["http://localhost:5000", "https://eveningswithpastorfaheem.netlify.app"];
+var corsOptions = { origin: whitelist, credentials: true };
+app.use(cors(corsOptions));
+
 // Define mongoose schemas and model for the collection
 const pastorVidSchema = new mongoose.Schema({
     name: String,
